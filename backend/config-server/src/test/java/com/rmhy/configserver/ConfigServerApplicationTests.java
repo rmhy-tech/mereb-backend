0d53bd727e5159c7ed6295178ccedab1fde61013
Leul Tewolde <leulwoldegabriel@gmail.com>
2024-09-12 11:19:34 -0500

    added multiple service and enhanced some features

---
+++ backend/config-server/src/test/java/com/rmhy/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmhy.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
